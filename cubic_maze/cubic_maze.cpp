fe45865cb31e9f0e239ebb810e2f47cb18bb3606
Regmont <karmodziregmont@gmail.com>
2023-06-24 00:37:07 +0300

    test graphics

--- cubic_maze/cubic_maze.cpp
+++ cubic_maze/cubic_maze.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 const int matr_size = 11;
+bool GameOver = false;
 
 enum Move { LEFT = 97, RIGHT = 100, UP = 119, DOWN = 115, TP_UP = 113, TP_DOWN = 101 };
 
@@ -30,50 +31,195 @@ void Recogn_Input(Move& player)
 		player = TP_DOWN;
 }
 
-void Print_Level(int matr[matr_size][matr_size], int x, int y)
+void Print_Wall(int matr[matr_size][matr_size], int y, int x)
 {
-	for (int i = 0; i < 11; i++)
+	int L = matr[x][y - 1];
+	int R = matr[x][y + 1];
+	int U = matr[x - 1][y];
+	int D = matr[x + 1][y];
+	int max_el = matr_size - 1;
+
+	if (x == 0 || y == 0 || x == max_el || y == max_el)
 	{
-		for (int j = 0; j < 11; j++)
+		if (x == 0 && y == 0)
+			cout << char(201);
+		else if (x == 0 && y == max_el)
+			cout << char(187);
+		else if (x == max_el && y == 0)
+			cout << char(200);
+		else if (x == max_el && y == max_el)
+			cout << char(188);
+		else if (x == 0)
+		{
+			if (D != 1)
+				cout << char(205);
+			else
+				cout << char(203);
+		}
+		else if (x == max_el)
+		{
+			if (U != 1)
+				cout << char(205);
+			else
+				cout << char(202);
+		}
+		else if (y == 0)
+		{
+			if (R != 1)
+				cout << char(186);
+			else
+				cout << char(204);
+		}
+		else
+		{
+			if (L != 1)
+				cout << char(186);
+			else
+				cout << char(185);
+		}
+	}
+	else
+	{
+		if (L != 1 && R == 1 && U != 1 && D == 1)
+			cout << char(201);
+		else if (L == 1 && R != 1 && U != 1 && D == 1)
+			cout << char(187);
+		else if (L == 1 && R != 1 && U == 1 && D != 1)
+			cout << char(188);
+		else if (L != 1 && R == 1 && U == 1 && D != 1)
+			cout << char(200);
+		else if (L != 1 && R != 1 && U == 1 && D == 1)
+			cout << char(186);
+		else if (L == 1 && R == 1 && U != 1 && D != 1)
+			cout << char(205);
+		else if (L != 1 && R == 1 && U == 1 && D == 1)
+			cout << char(204);
+		else if (L == 1 && R != 1 && U == 1 && D == 1)
+			cout << char(185);
+		else if (L == 1 && R == 1 && U != 1 && D == 1)
+			cout << char(203);
+		else if (L == 1 && R == 1 && U == 1 && D != 1)
+			cout << char(202);
+		else if (L == 1 && R == 1 && U == 1 && D == 1)
+			cout << char(206);
+		else if (L == 1 && R != 1 && U != 1 && D != 1)
+			cout << char(184);
+		else if (L != 1 && R == 1 && U != 1 && D != 1)
+			cout << char(213);
+		else if (L != 1 && R != 1 && U != 1 && D == 1)
+			cout << char(214);
+		else if (L != 1 && R != 1 && U == 1 && D != 1)
+			cout << char(211);
+		else
+			cout << char(197);
+	}
+}
+
+void Print_Level(int matr[matr_size][matr_size], int x, int y, int z)
+{
+	for (int i = 0; i < matr_size; i++)
+	{
+		for (int j = 0; j < matr_size; j++)
 			if (i == y && j == x)
-				cout << 'O';
+				cout << char(2);	//игрок
 			else
 				switch (matr[i][j])
 				{
-				case 0:
-				{
-					cout << ' ';
-					break;
-				}
-				case 1:
-				{
-					cout << '#';
-					break;
-				}
-				case 2:
-				{
-					cout << '^';
-					break;
-				}
-				case 3:
-				{
-					cout << '&';
-					break;
-				}
-				default:
-				{
-					cout << '?';
-					break;
-				}
+					case 0:
+					{
+						cout << ' ';	//пустое пространство
+						break;
+					}
+					case 1:
+					{
+						Print_Wall(matr, j, i);	//стена
+						break;
+					}
+					case 2:
+					{
+						cout << char(253);	//телепорт вверх
+						break;
+					}
+					case 3:
+					{
+						cout << char(15);	//телепорт вниз
+						break;
+					}
+					default:
+					{
+						cout << '?';
+						break;
+					}
 				}
 
 		cout << endl;
 	}
 }
 
+void Logic(int matr[matr_size][matr_size], int& x, int& y, int& z, Move player)
+{
+	while (_kbhit)
+	{
+		player = (Move)_getch();    //Считывание нажатой клавиши
+		Recogn_Input(player);
+
+		//		cout << player << ' ';
+
+		switch (player)
+		{
+			case LEFT:
+			{
+				if (matr[y][x - 1] != 1)
+					x--;
+
+				break;
+			}
+			case RIGHT:
+			{
+				if (matr[y][x + 1] != 1)
+					x++;
+
+				break;
+			}
+			case UP:
+			{
+				if (matr[y - 1][x] != 1)
+					y--;
+
+				break;
+			}
+			case DOWN:
+			{
+				if (matr[y + 1][x] != 1)
+					y++;
+
+				break;
+			}
+			case TP_UP:
+			{
+				//			if (matr[z][y][x] == 2)
+				//			z++;
+
+				break;
+			}
+			case TP_DOWN:
+			{
+				//			if (matr[z][y][x] == 3)
+				//			z--;
+
+				break;
+			}
+		}
+
+		system("cls");
+		Print_Level(matr, x, y, z);
+		//		cout << "x = " << x << " y = " << y << " z = " << z << endl;
+	}
+}
+
 int main()
 {
-	Move player;
+	Move player = Move(0);
 
 	//Пример матрицы с лабиринтом (массив будет трёхмерным)
 	int matr[matr_size][matr_size] =
@@ -93,63 +239,11 @@ int main()
 
 	int x = 9, y = 8, z = 0;   //правый нижний угол над цифрой 3
 
-	Print_Level(matr, x, y);
+	/*for (int i = 0; i < 256; i++)
+		cout << char(i) << " " << i << endl;*/
 
-	while (_kbhit)
-	{
-		player = (Move)_getch();    //Считывание нажатой клавиши
-		Recogn_Input(player);
-
-		//        cout << player << ' ';
-
-		switch (player)
-		{
-		case LEFT:
-		{
-			if (matr[y][x - 1] != 1)
-				x--;
-
-			break;
-		}
-		case RIGHT:
-		{
-			if (matr[y][x + 1] != 1)
-				x++;
-
-			break;
-		}
-		case UP:
-		{
-			if (matr[y - 1][x] != 1)
-				y--;
-
-			break;
-		}
-		case DOWN:
-		{
-			if (matr[y + 1][x] != 1)
-				y++;
-
-			break;
-		}
-		case TP_UP:
-		{
-			//                if (matr[z][y][x] == 2)
-			//                    z++;
-
-			break;
-		}
-		case TP_DOWN:
-		{
-			//                if (matr[z][y][x] == 3)
-								//z--;
-
-			break;
-		}
-		}
-
-		system("cls");
-		Print_Level(matr, x, y);
-		//        cout << "x = " << x << " y = " << y << " z = " << z << endl;
-	}
+	Print_Level(matr, x, y, z);
+	
+	while (!GameOver)
+		Logic(matr, x, y, z, player);
 }
